111d3001dbb105e9b1eefb643c7537ebd966fbdd
Julian De Vita <julianmdevita@gmail.com>
2025-03-02 21:22:41 -0500

    Added catch statements for custom attributes

--- custom_components/parcel/sensor.py
+++ custom_components/parcel/sensor.py
@@ -33,7 +33,7 @@ class RecentShipment(SensorEntity):
         self.coordinator = coordinator
         self._hass_custom_attributes = {}
         self._attr_name = "Recent Parcel Shipment"
-        self._attr_unique_id = "Recent_Parcel_Shipment"  ## prob should change
+        self._attr_unique_id = "Recent_Parcel_Shipment"
         self._globalid = "Recent_Parcel_Shipment"
         self._attr_icon = "mdi:package"
         self._attr_state = None
@@ -58,12 +58,24 @@ class RecentShipment(SensorEntity):
             if len(self._attr_name) > 20:
                 self._attr_name = self._attr_name[:20] + "..."
             self._attr_state = data[0]["events"][0]["event"]
+            try:
+                description = data[0]["description"]
+            except KeyError:
+                description = "Parcel"
+            try:
+                event_date = data[0]["events"][0]["date"]
+            except KeyError:
+                event_date = "Unknown"
+            try:
+                event_location = data[0]["events"][0]["location"]
+            except KeyError:
+                event_location = "Unknown"
 
             self._hass_custom_attributes = {
-                "full_description": data[0]["description"],
+                "full_description": description,
                 "tracking_number": data[0]["tracking_number"],
                 "status_code": data[0]["status_code"],
                 "carrier_code": data[0]["carrier_code"],
-                "event_date": data[0]["events"][0]["date"],
-                "event_location": data[0]["events"][0]["location"],
+                "event_date": event_date,
+                "event_location": event_location,
             }
