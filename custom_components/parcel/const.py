ba1becfd3648e05e72b577ad51a8968002583433
Julian De Vita <julianmdevita@gmail.com>
2025-03-02 22:33:38 -0500

    Fixed const domain var

--- custom_components/parcel/const.py
+++ custom_components/parcel/const.py
@@ -1,5 +1,5 @@
 """Constants for the Parcel integration."""
 
-DOMAIN = "parcel"
+DOMAIN = "parcel-ha"
 PARCEL_URL = "https://api.parcel.app/external/deliveries/"
 UPDATE_INTERVAL_SECONDS = 300  # RATE LIMIT IS 20 PER HOUR
